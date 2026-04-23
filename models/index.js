b3f1da4484b78c307edd552d18fd30320bb0827b
mohamedalmilawy-lab <mohamedalmilawy@gmail.com>
2026-04-23 19:12:37 +0300

    add database

--- models/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Register all Mongoose models in dependency order so refs/populate never hit MissingSchemaError.
- */
-require('./category.model');
-require('./product.model');
-require('./order.model');
-require('./user.model');
