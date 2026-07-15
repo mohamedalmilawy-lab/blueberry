fe9971eba5b00ff2cef670bede4784345aac01c9
mohamedalmilawy-lab <mohamedalmilawy@gmail.com>
2026-07-15 16:50:55 +0300

    fix note3

--- validators/push.validator.js
+++ validators/push.validator.js
@@ -2,7 +2,10 @@ const Joi = require('joi');
 
 const broadcastPushSchema = Joi.object({
     title: Joi.string().min(1).max(200).trim().required(),
-    message: Joi.string().min(1).max(2000).trim().required()
+    message: Joi.string().min(1).max(2000).trim().required(),
+    user: Joi.string().hex().length(24).messages({
+        'string.pattern.base': 'معرف غير صالح'
+    }).optional(),
 });
 
 module.exports = { broadcastPushSchema };
