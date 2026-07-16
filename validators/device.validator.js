660ef19af44a8c9b801f6954d32e5ee82b2d1f2b
mohamedalmilawy-lab <mohamedalmilawy@gmail.com>
2026-07-16 16:06:26 +0300

    add userid to device

--- validators/device.validator.js
+++ validators/device.validator.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
 
 const registerPushTokenSchema = Joi.object({
-    token: Joi.string().min(10).max(4096).trim().required()
+    token: Joi.string().min(10).max(4096).trim().required(),
+    userId: Joi.string().hex()
 });
 
 module.exports = { registerPushTokenSchema };
